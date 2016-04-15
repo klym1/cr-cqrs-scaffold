3c86ca01e94d708dd7a1a511f801e88b355fdca6
Rich Knight <rich@rich-knight.com>
2016-04-15 10:25:31 +0100

    Added logging

--- widget-api/CheckpointEventWriter/Program.cs
+++ widget-api/CheckpointEventWriter/Program.cs
@@ -15,7 +15,6 @@ namespace CheckpointEventWriter
             var conn = SetUpEventStoreConnection("EventStoreConnection");
             conn.AppendToStreamAsync("CheckpointRequests", ExpectedVersion.Any,
                 new EventData(Guid.NewGuid(), "CheckpointRequested", false, new byte[0], new byte[0])).Wait();
-
         }
 
         private static IEventStoreConnection SetUpEventStoreConnection(string connectionStringName)
@@ -25,9 +24,48 @@ namespace CheckpointEventWriter
             var settings =
                 ConnectionSettings.Create().KeepReconnecting().LimitRetriesForOperationTo(10);
             var eventStoreClient = EventStoreConnection.Create(settings, eventStoreConnectionString);
+            eventStoreClient.Closed += EventStoreClientOnClosed;
+            eventStoreClient.ErrorOccurred += EventStoreClientOnErrorOccurred;
+            eventStoreClient.Connected += EventStoreClientOnConnected;
+            eventStoreClient.Disconnected += EventStoreClientOnDisconnected;
+            eventStoreClient.Reconnecting += EventStoreClientOnReconnecting;
             eventStoreClient.ConnectAsync().Wait();
 
             return eventStoreClient;
+        }
+
+        private static void EventStoreClientOnReconnecting(object sender, ClientReconnectingEventArgs clientReconnectingEventArgs)
+        {
+            Console.WriteLine("Event Store reconnecting");
+        }
+
+        private static void EventStoreClientOnClosed(object sender, ClientClosedEventArgs clientClosedEventArgs)
+        {
+            Console.WriteLine("Event Store connection closed");
+        }
+
+        private static void EventStoreClientOnDisconnected(object sender, ClientConnectionEventArgs clientConnectionEventArgs)
+        {
+            Console.WriteLine(
+                $"Event Store disconnected from endpoint {clientConnectionEventArgs.RemoteEndPoint.Address}:{clientConnectionEventArgs.RemoteEndPoint.Port}");
+        }
+
+        private static void EventStoreClientOnConnected(object sender, ClientConnectionEventArgs clientConnectionEventArgs)
+        {
+            Console.WriteLine(
+                $"Event Store connected {clientConnectionEventArgs.RemoteEndPoint.Address}:{clientConnectionEventArgs.RemoteEndPoint.Port}");
+        }
+
+        private static void EventStoreClientOnErrorOccurred(object sender, ClientErrorEventArgs clientErrorEventArgs)
+        {
+            if (clientErrorEventArgs.Exception != null)
+            {
+                Console.WriteLine("Event Store error occurred: {0}", clientErrorEventArgs.Exception.Message);
+            }
+            else
+            {
+                Console.WriteLine("Unknown Event Store error");
+            }
         }
     }
 }
